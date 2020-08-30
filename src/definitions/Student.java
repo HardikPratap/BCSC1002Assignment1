5d19238de0171a4e1a563aa4fb60565753a1e9ef
HardikPratap <hardik.singh_cs19@gla.ac.in>
2020-08-30 13:41:28 +0530

    create constuctors for the fields in Student.java class

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -10,9 +10,26 @@ public class Student {
     private String firstNameOfStudent;
     private String middleNameOfStudent;
     private String lastNameOfStudent;
-    private String fullNameOfSudent;
+    private String fullNameOfStudent;
     private long studentUniversityRollNumber;
     private int numberOfBooksIssuedByStudent;
     private Book[] nameOfAllTheBooksIssuedByTheStudent;
+
+    public Student() {
+        firstNameOfStudent = "Hardik";
+        middleNameOfStudent = "Pratap";
+        lastNameOfStudent = "Singh";
+        studentUniversityRollNumber = 191500310;
+        numberOfBooksIssuedByStudent = 1;
+    }
+
+    public Student(String firstNameOfStudent, String middleNameOfStudent, String lastNameOfStudent, long studentUniversityRollNumber) {
+        this.firstNameOfStudent = firstNameOfStudent;
+        this.middleNameOfStudent = middleNameOfStudent;
+        this.lastNameOfStudent = lastNameOfStudent;
+        this.fullNameOfStudent = firstNameOfStudent + " " + middleNameOfStudent + " " + lastNameOfStudent;
+        this.studentUniversityRollNumber = studentUniversityRollNumber;
+    }
+
 
 }
