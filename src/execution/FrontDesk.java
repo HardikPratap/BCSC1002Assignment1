15250535ad8fb0243462e228196edb2539354b65
HardikPratap <hardik.singh_cs19@gla.ac.in>
2020-08-30 22:49:35 +0530

    create switch-case for the menu in FrontDesk.class

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,6 +6,9 @@
  * */
 package execution;
 
+import definitions.Book;
+import definitions.Student;
+
 import java.util.Scanner;
 
 public class FrontDesk {
@@ -15,14 +18,43 @@ public class FrontDesk {
     public static final int EXIT = 4;
 
     public static void main(String[] args) {
+        Book myBook = new Book();
+        Student mystudent = new Student();
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String studentName = scanner.next();
+        mystudent.setFullNameOfStudent(studentName);
         int userChoice;
         do {
 
             System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-" + "\n" + "How may I help you today?" + "\n" + "1. Issue a new book for me."
                     + "\n" + "2. Return a previously issues book for me." + "\n" + "3. Show me all my issues books." + "\n" + "4. Exit.\n");
             userChoice = scanner.nextInt();
+            switch (userChoice) {
+                case ISSUE_A_NEW_BOOK:
+                    System.out.println("Enter the name of Book you want to issue");
+                    scanner.nextLine();
+                    String bookName = scanner.nextLine();
+                    myBook.issueBook(bookName);
+                    break;
+                case RETURN_PREVIOUSLY_ISSUES_BOOK:
+                    System.out.println("Enter the name of the Book you want to return");
+                    String returnBookName = scanner.nextLine();
+                    scanner.nextLine();
+                    myBook.doReturn();
+                    break;
+                case SHOW_ALL_ISSUED_BOOK:
 
+                    String nameOfBooks = scanner.nextLine();
+                    mystudent.nameOfBooks();
+                    break;
+                case EXIT:
+                    System.out.println("Thanks For Using Front Desk!!!");
+                    break;
+                default:
+                    System.out.println("Wrong Option!!");
+
+            }
 
         } while (userChoice != EXIT);
     }
