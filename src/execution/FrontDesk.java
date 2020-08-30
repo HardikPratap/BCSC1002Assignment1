4876992209ee6b4a4bfa6a597d279f53ab5b528b
HardikPratap <hardik.singh_cs19@gla.ac.in>
2020-08-30 16:30:42 +0530

    create a do-while loop to repeat the menu untill user exit it

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -9,7 +9,21 @@ package execution;
 import java.util.Scanner;
 
 public class FrontDesk {
+    public static final int ISSUE_A_NEW_BOOK = 1;
+    public static final int RETURN_PREVIOUSLY_ISSUES_BOOK = 2;
+    public static final int SHOW_ALL_ISSUED_BOOK = 3;
+    public static final int EXIT = 4;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int userChoice;
+        do {
+
+            System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-" + "\n" + "How may I help you today?" + "\n" + "1. Issue a new book for me."
+                    + "\n" + "2. Return a previously issues book for me." + "\n" + "3. Show me all my issues books." + "\n" + "4. Exit.\n");
+            userChoice = scanner.nextInt();
+
+
+        } while (userChoice != EXIT);
     }
 }
